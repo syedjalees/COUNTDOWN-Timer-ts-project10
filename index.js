05886807a17fa60ec70bf67039d9dd47d1365e2d
SYED JALEES AKBAR SHAH <sayyedjalees958@gmail.com>
2024-06-13 01:22:47 +0500

    countdown timer done

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
-export {};
+#! /usr/bin/env node
+import inquirer from "inquirer";
+import { differenceInSeconds } from "date-fns";
+const res = await inquirer.prompt([
+    {
+        name: "userInput",
+        type: "number",
+        message: "Please enter the amount of second",
+        validate: (input) => {
+            if (isNaN(input)) {
+                return "Please enter valid number";
+            }
+            else if (input > 60) {
+                return "Seconds must be in 60";
+            }
+            else {
+                return true;
+            }
+        }
+    }
+]);
+let input = res.userInput;
+function startTime(val) {
+    const intTime = new Date().setSeconds(new Date().getSeconds() + val);
+    const intervalTime = new Date(intTime);
+    setInterval((() => {
+        const currTime = new Date();
+        const timeDiff = differenceInSeconds(intervalTime, currTime);
+        if (timeDiff <= 0) {
+            console.log("Timer has expired");
+            process.exit();
+        }
+        const min = Math.floor((timeDiff % (3600 * 24)) / 3600);
+        const sec = Math.floor(timeDiff % 60);
+        console.log(`${min.toString().padStart(2, "0")}:${sec.toString().padStart(2, "0")}`);
+    }), 1000);
+}
+startTime(input);
